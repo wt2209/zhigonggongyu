56bd41e76e595d9f983a7d59f5a1fea781e0596f
wt2209 <wt2209@126.com>
2020-08-14 14:33:46 +0800

    增加了博士学历，取消了其他学历

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -18,14 +18,14 @@ class Person extends Model
     const EDUCATION_COLLEGE = 1;
     const EDUCATION_BACHELOR = 2;
     const EDUCATION_MASTER = 3;
-    const EDUCATION_ELSE = 4;
+    const EDUCATION_DOCTOR = 4;
     const EDUCATION_UNKNOWN = 5;
 
     public static $educationMap = [
         self::EDUCATION_COLLEGE => '专科',
         self::EDUCATION_BACHELOR => '本科',
         self::EDUCATION_MASTER => '硕士',
-        self::EDUCATION_ELSE => '其他',
+        self::EDUCATION_DOCTOR => '博士',
         self::EDUCATION_UNKNOWN => '未知',
     ];
 
